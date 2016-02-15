b26932c046bd11a142099898c0025af8bc687ec7
Steven Atherton <steven@tarquindandy.com>
2016-02-15 12:35:31 +0000

    Update iframemanager__host.js

--- source/js/lib/news_special/iframemanager__host.js
+++ source/js/lib/news_special/iframemanager__host.js
@@ -202,8 +202,6 @@
                 this.data.hostPageCallback &&
                 (!this.iframeInstructionsRan)
             ) {
-                /* jshint evil:true */
-                (new Function(this.data.hostPageCallback)());
                 this.iframeInstructionsRan = true;
             }
         },
